24464720be486621b376f28cd9b759fa6b3af1c8
Immun4ik <mr.lehabear@mail.ru>
2025-05-28 13:08:34 +0300

    Initial commit: добавлен класс Team с итератором и генератором, настройка Webpack и ESLint

--- src/index.js
+++ src/index.js
@@ -1,36 +1,75 @@
+// класс персонажа (Character)
 class Character {
-    constructor(name, type) {
+    constructor({ name, type, health, level, attack, defence }) {
         this.name = name;
         this.type = type;
-        this.health = 50;
-        this.level = 1;
-        this.attack = 40;
-        this.defence = 10;
+        this.health = health;
+        this.level = level;
+        this.attack = attack;
+        this.defence = defence;
     }
 }
 
+// команда с итератором и генератором
 class Team {
     constructor() {
-        this.members = [];
+        this.members = new Set();
     }
 
     add(character) {
-        this.members.push(character);
+        if (!(character instanceof Character)) {
+            throw new Error('Можно добавлять только объекты Character');
+        }
+        this.members.add(character);
     }
 
-    *[Symbol.iterator]() {
+    // Итератор 
+    [Symbol.iterator]() {
+        const iterator = this.members.values();
+        return {
+            next() {
+                return iterator.next();
+            },
+        };
+    }
+
+    // Генератор (удобный итератор)
+    *generator() {
         for (const member of this.members) {
             yield member;
         }
     }
 }
 
+// Демонстрация
 
 const team = new Team();
-team.add(new Character('Лучник', 'Bowman'));
-team.add(new Character('Мечник', 'Swordsman'));
-team.add(new Character('Маг', 'Magician'));
 
-for (const character of team) {
-    console.log(character);
+const char1 = new Character({
+    name: 'Hunter',
+    type: 'Bowman',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+});
+const char2 = new Character({
+    name: 'Warrior',
+    type: 'Swordsman',
+    health: 60,
+    level: 1,
+    attack: 30,
+    defence: 20,
+});
+team.add(char1);
+team.add(char2);
+
+console.log('Итератор:');
+for (const member of team) {
+    console.log(member);
+}
+
+console.log('Генератор:');
+for (const member of team.generator()) {
+    console.log(member);
 }
